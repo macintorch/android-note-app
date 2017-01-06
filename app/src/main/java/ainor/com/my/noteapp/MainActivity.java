300cbfa2be6b1e4f3a3ad09e0ddd727ed91dd215
Ainor Syahrizal <macintorch@gmail.com>
2017-01-06 17:53:35 +0800

    listview setup

--- app/src/main/java/ainor/com/my/noteapp/MainActivity.java
+++ app/src/main/java/ainor/com/my/noteapp/MainActivity.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,9 +30,23 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    ArrayList<String> notes = new ArrayList<>();
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.notesListView);
+
+        notes.add("Example note");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, notes);
+
+        listView.setAdapter(arrayAdapter);
+
+
     }
 }
