c4ed016a71f28c2b2d11772f7fa33b3eaed4ca73
Ainor Syahrizal <macintorch@gmail.com>
2017-01-06 17:02:07 +0800

    add menu inflater

--- app/src/main/java/ainor/com/my/noteapp/MainActivity.java
+++ app/src/main/java/ainor/com/my/noteapp/MainActivity.java
@@ -2,8 +2,29 @@ package ainor.com.my.noteapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+
+        menuInflater.inflate(R.menu.main_menu,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
